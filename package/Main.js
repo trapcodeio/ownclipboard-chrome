e8f69892d948c66c5b41b1eaab2f5033b8831951
trapcodeio <trapcode.io@gmail.com>
2020-12-03 07:46:01 +0100

    Not using tabs

--- package/Main.js
+++ package/Main.js
@@ -1,8 +1,8 @@
 import {
-	getConfig,
-	getter,
-	readClipboard,
-	updateConfig,
+  getConfig,
+  getter,
+  readClipboard,
+  updateConfig,
 } from './functions/chrome.fn.js';
 import {chromeStore} from './WebStore.js';
 import {html_formatted} from './functions/lean.fn.js';
@@ -18,113 +18,113 @@ let timeout = null;
  * @returns {Promise<void>}
  */
 export async function main() {
-	/**
-	 * @type {import('./config.js')|unknown}
-	 */
-	const config = await getConfig();
-	
-	// If Watch is enabled in config then watch clipboard
-	// noinspection JSUnresolvedVariable
-	if (config && config.clips.watch) {
-		keepWatching = true;
-		watchClipboard(config.clips.interval);
-	}
+  /**
+   * @type {import('./config.js')|unknown}
+   */
+  const config = await getConfig();
+  
+  // If Watch is enabled in config then watch clipboard
+  // noinspection JSUnresolvedVariable
+  if (config && config.clips.watch) {
+    keepWatching = true;
+    watchClipboard(config.clips.interval);
+  }
 }
 
 export async function checkDaemon() {
-	const config = await getConfig();
-	
-	// If Watch is enabled in config then watch clipboard
-	// noinspection JSUnresolvedVariable
-	if (config && config.clips.watch) {
-		keepWatching = true;
-		watchClipboard(config.clips.interval);
-	}
+  const config = await getConfig();
+  
+  // If Watch is enabled in config then watch clipboard
+  // noinspection JSUnresolvedVariable
+  if (config && config.clips.watch) {
+    keepWatching = true;
+    watchClipboard(config.clips.interval);
+  }
 }
 
 export async function startWatch() {
-	const config = await getConfig();
-	
-	// Update Config
-	await updateConfig({
-		'clips.watch': true,
-	});
-	
-	keepWatching = true;
-	watchClipboard(config.clips.interval);
-	
-	console.log('Now watching!');
+  const config = await getConfig();
+  
+  // Update Config
+  await updateConfig({
+    'clips.watch': true,
+  });
+  
+  keepWatching = true;
+  watchClipboard(config.clips.interval);
+  
+  console.log('Now watching!');
 }
 
 export async function stopWatching() {
-	clearTimeout(timeout);
-	keepWatching = false;
-	
-	await updateConfig({
-		'clips.watch': false,
-	});
-	
-	console.log('Stopped watching.');
+  clearTimeout(timeout);
+  keepWatching = false;
+  
+  await updateConfig({
+    'clips.watch': false,
+  });
+  
+  console.log('Stopped watching.');
 }
 
 export function watchClipboard(interval) {
-	if (keepWatching) {
-		if (timeout !== null) clearTimeout(timeout);
-		
-		timeout = setTimeout(() => {
-			/**
-			 * Check if clipboard has changed
-			 */
-			if (clipboard.hasChanged()) {
-				clipBoardHasChanged().
-						then(() => watchClipboard(interval)).
-						catch(console.error);
-			} else {
-				watchClipboard(interval);
-			}
-		}, interval * 1000);
-	}
+  if (keepWatching) {
+    if (timeout !== null) clearTimeout(timeout);
+    
+    timeout = setTimeout(() => {
+      /**
+       * Check if clipboard has changed
+       */
+      if (clipboard.hasChanged()) {
+        clipBoardHasChanged().
+            then(() => watchClipboard(interval)).
+            catch(console.error);
+      } else {
+        watchClipboard(interval);
+      }
+    }, interval * 1000);
+  }
 }
 
 export async function clipBoardHasChanged() {
-	clipboard.updateValue();
-	let clip = clipboard.value;
-	
-	// Validate new clip
-	if (!clip || typeof clip !== 'string' ||
-			(clip && !clip.trim().length)) return;
-	
-	// Trim CLip
-	clip = clip.trim();
-	
-	// Get All Local Clips
-	const localClips = await chromeStore.get('localClips');
-	let data = localClips ? localClips : [];
-	
-	// Check if clip exists in local
-	const existingContentIndex = _.findIndex(data, d => d.content === clip);
-	
-	// if clip has content
-	if (existingContentIndex >= 0) {
-		// sort content!
-		data[existingContentIndex].last_copied = new Date().toISOString();
-		data = _.sortBy(data, ['last_copied']);
-		data.reverse();
-	} else {
-		const config = await getConfig();
-		
-		if (data.length >= config.perPage.local) {
-			data.pop();
-		}
-		
-		data.unshift({
-			content: clip,
-			html_formatted: html_formatted(clip),
-			last_copied: new Date().toISOString(),
-			created_at: new Date().toISOString(),
-		});
-	}
-	
-	// Save new data to local store.
-	await chromeStore.setSync({localClips: data});
+  clipboard.updateValue();
+  let clip = clipboard.value;
+  
+  // Validate new clip
+  if (!clip || typeof clip !== 'string' ||
+      (clip && !clip.trim().length)) return;
+  
+  // Trim CLip
+  clip = clip.trim();
+  
+  // Get All Local Clips
+  const localClips = await chromeStore.get('localClips');
+  let data = localClips ? localClips : [];
+  
+  // Check if clip exists in local
+  const existingContentIndex = _.findIndex(data, d => d.content === clip);
+  
+  // if clip has content
+  if (existingContentIndex >= 0) {
+    // sort content!
+    data[existingContentIndex].last_copied = new Date().toISOString();
+    data = _.sortBy(data, ['last_copied']);
+    data.reverse();
+  } else {
+    const config = await getConfig();
+    
+    if (data.length >= config.perPage.local) {
+      data.pop();
+    }
+    
+    data.unshift({
+      content: clip,
+      html_formatted: html_formatted(clip),
+      last_copied: new Date().toISOString(),
+      created_at: new Date().toISOString(),
+    });
+  }
+  
+  // Save new data to local store.
+  await chromeStore.setSync({localClips: data});
 }
